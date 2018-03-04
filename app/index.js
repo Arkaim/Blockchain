eae58140d032bc58d81e86f33df9b4b19998af8f
Arkaim <kaimbayev@gmail.com>
2018-03-05 00:23:05 +0600

    synchronizing the blockchain across peers

--- app/index.js
+++ app/index.js
@@ -18,7 +18,7 @@ app.get('/blocks', (req,res) => {
 app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data);
   console.log(`New block added ${block.toString()}`);
-
+  p2pServer.syncChains();
   res.redirect('/blocks');
 })
 
--- app/p2p-server.js
+++ app/p2p-server.js
@@ -30,14 +30,24 @@ class P2pServer {
     console.log('Socket added');
 
     this.messageHandler(socket);
-
-    socket.send(JSON.stringify(this.blockchain.chain));
+    this.sendChain(socket);
   }
 
   messageHandler(socket) {
     socket.on('message', message => {
       const data = JSON.parse(message);
       console.log('data', data);
+      this.blockchain.replaceChain(data);
+    });
+  }
+
+  sendChain (socket) {
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  syncChains() {
+    this.sockets.forEach(socket => {
+      this.sendChain(socket)
     })
   }
 }
