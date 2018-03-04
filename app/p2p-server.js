56db6e32203cea870d523f0ea3cfbd7f419dec21
Arkaim <kaimbayev@gmail.com>
2018-03-04 22:59:08 +0600

    handle messages from peers

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -28,6 +28,17 @@ class P2pServer {
   connectSocket (socket) {
     this.sockets.push(socket);
     console.log('Socket added');
+
+    this.messageHandler(socket);
+
+    socket.send(JSON.stringify(this.blockchain.chain));
+  }
+
+  messageHandler(socket) {
+    socket.on('message', message => {
+      const data = JSON.parse(message);
+      console.log('data', data);
+    })
   }
 }
 
